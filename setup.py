fb315aba66f9853c16799dab1dfb806490aaa6d7
Carl <carl.verstraete@studioemma.com>
2019-08-12 11:40:36 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name="edig",
+    author="Carl Verstraete",
+    description=("Extended dig program."),
+    license="MIT",
+    packages=find_packages(),
+    scripts=['edig'],
+    install_requires=['dnspython', 'geoip2', 'tld','ipwhois'],
+    python_requires='>3.4',
+)
